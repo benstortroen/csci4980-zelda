dfae7efc5b2baea728ccbdf9f9e42f0b5e5f07f0
stort040 <stort040@umn.edu>
2026-09-21 02:28:36 -0500

    Add player movement

---
+++ Assets/Scripts/ArrowKeyMovement.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+
+public class ArrowKeyMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float movement_speed = 4;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 current_input = GetInput();
+        rb.linearVelocity = current_input * movement_speed;
+    }
+
+    Vector2 GetInput()
+    {
+        float horizontal_input = Input.GetAxisRaw("Horizontal");
+        float vertical_input = Input.GetAxisRaw("Vertical");
+
+        if (horizontal_input != 0.0f)
+        {
+            vertical_input = 0.0f;
+        }
+
+        return new Vector2 (horizontal_input, vertical_input);
+    }
+}
