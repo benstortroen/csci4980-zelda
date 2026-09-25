d9a7f81876ca6b7a47263f54e68ffe0a0ad1752d
stort040 <stort040@umn.edu>
2026-09-25 10:43:13 -0500

    Change grid-based movement to snap to nearest grid cell, rather than gradually moving to nearest grid cell

--- Assets/Scripts/ArrowKeyMovement.cs
+++ Assets/Scripts/ArrowKeyMovement.cs
@@ -2,6 +2,7 @@ using System;
 using Unity.Mathematics;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 public class ArrowKeyMovement : MonoBehaviour
@@ -20,41 +21,54 @@ public class ArrowKeyMovement : MonoBehaviour
     void Update()
     {
         Vector2 current_input = GetInput();
+        AlignWithAxis(current_input);
         // Set speed
         rb.linearVelocity = current_input * movement_speed;
-
-        //Temp version of transform to make editing easier
-        DirectionalCenter(current_input);
+        
     }
 
-    private void DirectionalCenter(Vector2 input)
+    // Snap player to the opposite axis they are moving along
+    // if player is moving horizontally, snap to nearest y grid
+    // if player is moving vertically, snap to nearest x grid
+    // Each grid step is 0.5 units
+    private void AlignWithAxis(Vector2 input)
     {
         Vector3 temp_pos = transform.position;
+        
+        // Player is moving along x axis
         if (input.x != 0)
         {
-            // Debug.Log("Locking");
-            float offset = temp_pos.y % 1f;
-            if (offset <= .5f)
+            float offset = temp_pos.y % 0.5f;
+            
+            // snap to nearest lower grid position
+            temp_pos.y -= offset;
+            
+            // check if player was closer to nearest higher grid position
+            // move to nearest higher grid position
+            if (offset > 0.25)
             {
-                temp_pos.y -= adjust_rate * Time.deltaTime;
-            }
-            else
-            {
-                temp_pos.y += adjust_rate * Time.deltaTime;
+                temp_pos.y += 0.5f; 
             }
         }
-        else if (input.y != 0)
+        
+        // Player is moving along y axis
+        if (input.y != 0)
         {
-            float offset = temp_pos.x % 1f;
-            if (offset <= .5f)
+            float offset = temp_pos.x % 0.5f;
+            
+            // snap to nearest lower grid position
+            temp_pos.x -= offset;
+            
+            // check if player was closer to nearest higher grid position
+            // move to nearest higher grid position
+            if (offset > 0.25)
             {
-                temp_pos.x -= adjust_rate * Time.deltaTime;
-            }
-            else
-            {
-                temp_pos.x += adjust_rate * Time.deltaTime;
+                temp_pos.x += 0.5f; 
             }
         }
+
+        Debug.Log(temp_pos);
+
         transform.position = temp_pos;
     }
     Vector2 GetInput()
@@ -62,9 +76,14 @@ public class ArrowKeyMovement : MonoBehaviour
         float horizontal_input = Input.GetAxisRaw("Horizontal");
         float vertical_input = Input.GetAxisRaw("Vertical");
 
-        if (horizontal_input != 0.0f)
+        // Only allow one axis of movement at a time
+        if (horizontal_input != 0f)
         {
-            vertical_input = 0.0f;
+            vertical_input = 0f;
+        }
+        if (vertical_input != 0f)
+        {
+            horizontal_input = 0f;
         }
 
         return new Vector2(horizontal_input, vertical_input);
