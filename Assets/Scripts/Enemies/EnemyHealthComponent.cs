65639eb10cba901420ca0e04ae6ea30021f4cb14
Luke Golobitsh <golob022@umn.edu>
2026-09-25 20:27:22 -0500

    Item Drop Logic

--- Assets/Scripts/Enemies/EnemyHealthComponent.cs
+++ Assets/Scripts/Enemies/EnemyHealthComponent.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class EnemyHealthComponent : HealthComponent
 {
 
+    [SerializeField] private GameObject rupee;
+    [SerializeField] private GameObject heart;
     void Start()
     {
         max_hp = 3f;
@@ -10,6 +12,16 @@ public class EnemyHealthComponent : HealthComponent
     }
     public override void OnDeath()
     {
+
+        int drop = Random.Range(0, 5);
+        if (drop == 0)
+        {
+            Instantiate(rupee, transform.position, Quaternion.identity);
+        }
+        else if (drop == 1)
+        {
+            Instantiate(heart, transform.position, Quaternion.identity);
+        }
         gameObject.SetActive(false);
     }
 
