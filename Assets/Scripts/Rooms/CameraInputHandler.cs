3312b893785e0cd357ffc5614abee70140efa3d0
Luke Golobitsh <golob022@umn.edu>
2026-09-25 20:10:32 -0500

    Potential settup for camera movement but I'm not sure

--- Assets/Scripts/Rooms/CameraInputHandler.cs
+++ Assets/Scripts/Rooms/CameraInputHandler.cs
@@ -5,17 +5,11 @@ using UnityEngine.InputSystem;
 public class CameraInputHandler : MonoBehaviour
 {
     MessengerComponent messenger;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         messenger = gameObject.GetComponent<MessengerComponent>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        handleInput();
     }
 
     void handleInput()
@@ -28,17 +22,25 @@ public class CameraInputHandler : MonoBehaviour
         if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
         {
             messenger.send(new ArrowInputMessage(this, ArrowInputMessage.LEFT_ARROW));
-            
+
         }
         if (Keyboard.current.upArrowKey.wasPressedThisFrame)
         {
             messenger.send(new ArrowInputMessage(this, ArrowInputMessage.UP_ARROW));
-            
+
         }
         if (Keyboard.current.downArrowKey.wasPressedThisFrame)
         {
             messenger.send(new ArrowInputMessage(this, ArrowInputMessage.DOWN_ARROW));
-            
+
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "door")
+        {
+            handleInput();
         }
     }
 }
